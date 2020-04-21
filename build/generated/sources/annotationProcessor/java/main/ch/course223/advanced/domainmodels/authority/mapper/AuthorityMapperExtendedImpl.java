912376c891cb4d9298c6415760ccf3aac97a7c77
Sabrina Westhauser <sabrina.westhauser@edu.tbz.ch>
2020-04-21 17:12:20 +0200

    BIITE

--- build/generated/sources/annotationProcessor/java/main/ch/course223/advanced/domainmodels/authority/mapper/AuthorityMapperExtendedImpl.java
+++ build/generated/sources/annotationProcessor/java/main/ch/course223/advanced/domainmodels/authority/mapper/AuthorityMapperExtendedImpl.java
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-04-14T16:17:29+0200",
-    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 13.0.2 (Oracle Corporation)"
+    date = "2020-04-21T17:01:30+0200",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.2 (Oracle Corporation)"
 )
 @Component
 public class AuthorityMapperExtendedImpl implements AuthorityMapperExtended {
--- build/generated/sources/annotationProcessor/java/main/ch/course223/advanced/domainmodels/user/mapper/UserMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/ch/course223/advanced/domainmodels/user/mapper/UserMapperImpl.java
@@ -15,8 +15,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-04-14T16:17:29+0200",
-    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 13.0.2 (Oracle Corporation)"
+    date = "2020-04-21T17:01:30+0200",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.2 (Oracle Corporation)"
 )
 @Component
 public class UserMapperImpl implements UserMapper {
