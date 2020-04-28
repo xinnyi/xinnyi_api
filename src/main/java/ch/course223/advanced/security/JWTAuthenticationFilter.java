ce2bad2a1367043fb25834e78018c327297e9581
klemens <klemens.raduner@nyp.ch>
2020-04-28 13:58:43 +0200

    use telegram user id from header as principal instead of jwt

--- src/main/java/ch/course223/advanced/security/JWTAuthenticationFilter.java
+++ src/main/java/ch/course223/advanced/security/JWTAuthenticationFilter.java
@@ -23,58 +23,56 @@ import java.util.Date;
 
 class JWTAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
 
-	private PropertyReader propertyReader;
-	private UserMapper userMapper;
+    private PropertyReader propertyReader;
+    private UserMapper userMapper;
 
-	JWTAuthenticationFilter(
-			RequestMatcher requiresAuthenticationRequestMatcher,
-			AuthenticationManager authenticationManager,
-			PropertyReader propertyReader,
-			UserMapper userMapper
-	) {
-		super(requiresAuthenticationRequestMatcher);
-		setAuthenticationManager(authenticationManager);
-		this.propertyReader = propertyReader;
-		this.userMapper = userMapper;
-	}
+    JWTAuthenticationFilter(
+            RequestMatcher requiresAuthenticationRequestMatcher,
+            AuthenticationManager authenticationManager,
+            PropertyReader propertyReader,
+            UserMapper userMapper
+    ) {
+        super(requiresAuthenticationRequestMatcher);
+        setAuthenticationManager(authenticationManager);
+        this.propertyReader = propertyReader;
+        this.userMapper = userMapper;
+    }
 
-	@Override
-	public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res)
-			throws AuthenticationException, IOException, ServletException {
-		try {
-			User creds = new ObjectMapper().readValue(req.getInputStream(), User.class);
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res) throws AuthenticationException, IOException, ServletException {
+        try {
+            User creds = new ObjectMapper().readValue(req.getInputStream(), User.class);
 
-			return getAuthenticationManager()
-					.authenticate(new UsernamePasswordAuthenticationToken(creds.getEmail(), creds.getPassword()));
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+            return getAuthenticationManager()
+                    .authenticate(new UsernamePasswordAuthenticationToken(creds.getEmail(), creds.getPassword()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	@Override
-	protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain,
-                                            Authentication auth) throws IOException, ServletException {
-		UserDetailsImpl userDetailsImpl = (UserDetailsImpl) auth.getPrincipal();
-		User user = userDetailsImpl.getUser();
-		String subject = user.getId();
+    @Override
+    protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain, Authentication auth) throws IOException, ServletException {
+        UserDetailsImpl userDetailsImpl = (UserDetailsImpl) auth.getPrincipal();
+        User user = userDetailsImpl.getUser();
+        String subject = user.getId();
 
-		String token = Jwts.builder().setSubject(subject)
-				.setExpiration(
-						new Date(System.currentTimeMillis() + propertyReader.getIntProperty("jwt.expiration-time")))
-				.signWith(SignatureAlgorithm.HS512, propertyReader.getStringProperty("jwt.secret").getBytes())
-				.setIssuer(propertyReader.getStringProperty("jwt.issuer"))
-				.compact();
-		res.addHeader(propertyReader.getStringProperty("jwt.header-string"),
-				propertyReader.getStringProperty("jwt.token-prefix") + token);
-		res.addHeader("Access-Control-Expose-Headers", propertyReader.getStringProperty("jwt.header-string") );
+        String token = Jwts.builder().setSubject(subject)
+                .setExpiration(
+                        new Date(System.currentTimeMillis() + propertyReader.getIntProperty("jwt.expiration-time")))
+                .signWith(SignatureAlgorithm.HS512, propertyReader.getStringProperty("jwt.secret").getBytes())
+                .setIssuer(propertyReader.getStringProperty("jwt.issuer"))
+                .compact();
+        res.addHeader(propertyReader.getStringProperty("jwt.header-string"),
+                propertyReader.getStringProperty("jwt.token-prefix") + token);
+        res.addHeader("Access-Control-Expose-Headers", propertyReader.getStringProperty("jwt.header-string"));
 
-		UserDTO userDTO = userMapper.toDTO(user);
+        UserDTO userDTO = userMapper.toDTO(user);
 
-		String userDTOString = new ObjectMapper().writeValueAsString(userDTO);
+        String userDTOString = new ObjectMapper().writeValueAsString(userDTO);
 
-		res.getWriter().write(userDTOString);
+        res.getWriter().write(userDTOString);
 
-		res.setContentType("application/json");
-	}
+        res.setContentType("application/json");
+    }
 
 }
