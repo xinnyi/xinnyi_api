09ce80c9ad33d559605a21c05ff685a43f73f3cd
Sabrina Westhauser <sabrina.westhauser@edu.tbz.ch>
2020-05-06 14:53:57 +0200

    some updates

--- build/generated/sources/annotationProcessor/java/main/ch/course223/advanced/domainmodels/role/mapper/RoleMapperExtendedImpl.java
+++ build/generated/sources/annotationProcessor/java/main/ch/course223/advanced/domainmodels/role/mapper/RoleMapperExtendedImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-05-06T14:21:34+0200",
+    date = "2020-05-06T14:39:49+0200",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.2 (Oracle Corporation)"
 )
 @Component
